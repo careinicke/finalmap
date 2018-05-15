60639506802833954e38fc2179c18c54ab9a6e72
careinicke <35277845+careinicke@users.noreply.github.com>
2018-05-15 19:25:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+
+  $(".menu").on("change", function(event){
+    var choice = $(this).val();
+
+    if(choice=="low"){
+         $(".item.active").removeClass("active")
+      $("."+choice).addClass("less");
+    }
+
+    else if(choice=="middle"){
+         $(".item.less").removeClass("less")
+      $("."+choice).addClass("more")
+    }
+    
+    else if(choice=="high"){
+         $(".item.more").removeClass("more")
+       $("."+choice).addClass("highlight")
+    }
+    
+    // else (choice=="all")
+    //   $(".item.highlight").removeClass("active")
+    // $(".").addClass("more","less")
+
+  })
+})
+
+
+$("path, circle").hover(function(e) {
+  $('#info-box').css('display','block');
+  $('#info-box').html($(this).data('info'));
+});
+
+$("path, circle").mouseleave(function(e) {
+  $('#info-box').css('display','none');
+});
+
+$(document).mousemove(function(e) {
+  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+}).mouseover();
+
+var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if(ios) {
+  $('a').on('click touchend', function() {
+    var link = $(this).attr('href');
+    window.open(link,'_blank');
+    return false;
+  });
+}
+
