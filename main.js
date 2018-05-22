31a12a739d3fcd4eb5dde0b4609d910412e19aff
careinicke <35277845+careinicke@users.noreply.github.com>
2018-05-22 17:03:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,68 @@
-
 $(document).ready(function(){
 
   $(".menu").on("change", function(event){
     var choice = $(this).val();
 
     if(choice=="low"){
-         $(".item.active").removeClass("active")
+         $(".item.active").removeClass("active highlight less more")
+         $(".item").removeClass("highlight")
+         $(".item").removeClass("less")
+         $(".item").removeClass("more")
       $("."+choice).addClass("less");
     }
 
     else if(choice=="middle"){
-         $(".item.less").removeClass("less")
+         $(".item").removeClass("less")
+         $(".item").removeClass("active")
+         $(".item").removeClass("highlight")
       $("."+choice).addClass("more")
     }
     
     else if(choice=="high"){
-         $(".item.more").removeClass("more")
+         $(".item").removeClass("more")
+         $(".item").removeClass("less")
+         $(".item").removeClass("active")
        $("."+choice).addClass("highlight")
     }
-    
+
+    // else (choice=="all")
+    //   $(".item").removeClass("more")
+    //  $(".item").removeClass("less")
+    //  $(".item").removeClass("active")
+    //  $(".item").removeClass("highlight")
+    //  $("."+choice).addClass("image")
     // else (choice=="all")
     //   $(".item.highlight").removeClass("active")
     // $(".").addClass("more","less")
 
   })
 })
+// $(document).ready(function(){
+
+//   $(".menu").on("change", function(event){
+//     var choice = $(this).val();
+
+//     if(choice=="low"){
+//          $(".item.active").removeClass("active")
+//       $("."+choice).addClass("less");
+//     }
+
+//     else if(choice=="middle"){
+//          $(".item.less").removeClass("less")
+//       $("."+choice).addClass("more")
+//     }
+    
+//     else if(choice=="high"){
+//          $(".item.more").removeClass("more")
+//        $("."+choice).addClass("highlight")
+//     }
+    
+//     // else (choice=="all")
+//     //   $(".item.highlight").removeClass("active")
+//     // $(".").addClass("more","less")
+
+//   })
+// })
 
 
 $("path, circle").hover(function(e) {
